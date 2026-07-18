fc1977be9b851dfad7da1750b02fc01145358ce5
shivam1108-06 <shivamkotak118@gmail.com>
2026-07-18 17:21:24 +0530

    Setup React Router, Axios and Authentication Context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
